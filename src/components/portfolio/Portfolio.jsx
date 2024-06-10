1dafae0747dabb45332ad7273d1268ca3b4b6cd3
Dennis Wells <56503655+Dennis231708@users.noreply.github.com>
2024-06-10 11:22:31 -0500

    updated decoder ring disc
    updated decoder ring disc

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -14,7 +14,7 @@ const items = [
     id: 2,
     title: "Decoder Ring",
     img: "/DecoderRingss.png",
-    desc: "This project involved working on complex algorithms and unit testing with Mocha and Chai. Its purpose is to allow users to encode or decode messages using one of three methods: Caesar’s shift, the Polybius square, or the substitution cipher. In Caesar’s shift, the program shifts letters down or up the alphabet by a specified amount to encode or decode messages, respectively. The Polybius square method translates letters into numbers or vice versa based on their positions within a two-dimensional chart. Lastly, the substitution cipher exchanges letters from the English alphabet with those provided by the user, resulting in encoded or decoded messages. This project provided an opportunity to explore encryption techniques and enhance skills in algorithmic problem-solving and testing methodologies.",
+    desc: "This project involved developing complex algorithms, using Mocha and Chai for unit testing. It enables users to encode or decode messages using three methods: Caesar’s shift, the Polybius square, or substitution cipher. Caesar’s shift moves letters up or down the alphabet, Polybius square translates letters into numbers, and substitution cipher exchanges letters with user-provided ones. It offered an opportunity to explore encryption techniques and improve algorithmic problem-solving and testing skills.",
     link: "https://dwdecoderring.netlify.app/",
   },
   {
